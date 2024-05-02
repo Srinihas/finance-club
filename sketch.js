705f39f8e9bf9f09e21e4b5b8246edf00240086f
Srinihas <115796290+Srinihas@users.noreply.github.com>
2024-05-02 17:19:38 +0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  database = firebase.database();
+  database.ref().once('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var key = childSnapshot.key;
+      clubArray.push(key);
+    });
+    // Create scrollable lists for each club
+    createScrollableLists();
+  });
+}
+
+function draw() {
+  clubArray.forEach(element => {
+    let drift = random(-0.1, 0.1); // Drift in the stock price
+    let noise = randomGaussian(0, volatility); // Random noise
+    let deltaPrice = drift * initialPrice * timeInterval + noise; // Change in stock price
+
+    // Update stock price for each club
+    let stock_price = database.ref(`/clubs/${element.trim()}/stock-price`);
+    stock_price.set(deltaPrice);
+  });
+}
